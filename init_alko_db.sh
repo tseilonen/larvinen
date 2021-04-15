f9932ca7b83fbe5598a86bb7a607090ae8ebff98
tseilonen <tino.seilonen@gmail.com>
2021-04-15 18:28:05 +0300

    Redirect stderr to devnull from init_alko_db.sh

--- init_alko_db.sh
+++ init_alko_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mv data/alkon-hinnasto-tekstitiedostona.xlsx data/alkon-hinnasto-tekstitiedostona.xlsx.backup
-mv data/alko.db data/alko.db.backup
+mv data/alkon-hinnasto-tekstitiedostona.xlsx data/alkon-hinnasto-tekstitiedostona.xlsx.backup 2> /dev/null
+mv data/alko.db data/alko.db.backup 2> /dev/null
 curl -sO "https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xlsx" > /dev/null
-mv alkon-hinnasto-tekstitiedostona.xlsx data/
-python larvinen/alko.py
+mv alkon-hinnasto-tekstitiedostona.xlsx data/ 2> /dev/null
+python larvinen/alko.py 2> /dev/null
